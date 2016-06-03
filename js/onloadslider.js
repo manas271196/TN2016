29912ea77222970307b4c1b86a708dd9f85e43e1
manas271196 <sharma.manas271196@gmail.com>
2016-06-03 14:13:19 +0530

    Update onloadslider.js

--- js/onloadslider.js
+++ js/onloadslider.js
@@ -70,8 +70,8 @@ function loadall(){
                            //debugger;
 						   var mouse=d3.mouse(d3.select(this).node());
                            tooltip.classed('hidden', false)
-                                .attr('style', 'left:' + (mouse[0] + 160) +
-                                        'px; top:' + (mouse[1] +30) + 'px')
+                                .attr('style', 'left:' + (mouse[0] + 190) +
+                                        'px; top:' + (mouse[1] +60) + 'px')
 
                                 .html("<b>"+d.properties.AC_NAME+"</b>"+"\nMargin: "+margin_data[+(d.properties.AC_NO)-1].Margin+"\nWinner: "+win[+(d.properties.AC_NO)][2016]);
                            })
